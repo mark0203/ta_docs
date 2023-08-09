d36e5a9188abc6c643cf65fa1df535187bc0759a
Mark <mark0203@gmail.com>
2023-08-09 22:54:16 +0200

    Cypress typescript empty framework (#3)
    * Update documentation with some examples
    
    * Forgot to convert the config file to typescript and set the config to strict
    
    * Some minor adjustments
    
    ---------
    
    Co-authored-by: Mark Duivesteijn

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,8 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
