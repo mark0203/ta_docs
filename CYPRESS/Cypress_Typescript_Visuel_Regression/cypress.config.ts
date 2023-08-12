906c6c3e7439b7e64d35d710407a37aba8eaa3ad
Mark Duivesteijn <mark0203+truenasgitea@gmail.com>
2023-08-12 23:29:11 +0200

    Added forgotten cypress.config.ts file

---
+++ CYPRESS/Cypress_Typescript_Visuel_Regression/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress';
+import { initPlugin } from "@frsource/cypress-plugin-visual-regression-diff/plugins";
+
+export default defineConfig({
+  video: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.env.CYPRESS_INCLUDE_TAGS = 'all';
+      config.env.CYPRESS_EXCLUDE_TAGS = 'two,three';
+
+      on('file:preprocessor', tagify(config));
+    },
+
+    baseUrl: 'https://example.cypress.io',
+  },
+});
