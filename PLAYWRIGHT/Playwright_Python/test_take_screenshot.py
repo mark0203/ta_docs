3ace9876c1583e2b5941b4aee04b7bfb43e4b867
Mark Duivesteijn <?>
2024-01-26 23:45:24 +0100

    Moved screenshots to seperate folder

--- PLAYWRIGHT/Playwright_Python/test_take_screenshot.py
+++ PLAYWRIGHT/Playwright_Python/test_take_screenshot.py
@@ -3,12 +3,12 @@ from playwright.sync_api import Page, expect
 
 def test_page_matches_screenshot(page: Page):
     page.goto("https://playwright.dev/")
-    page.screenshot(path="page.png")
-    page.screenshot(path="fullPage.png", full_page=True)
+    page.screenshot(path="screenshots/page.png")
+    page.screenshot(path="screenshots/fullPage.png", full_page=True)
 
 def test_footer_matches_screenshot(page: Page):
     page.goto("https://playwright.dev/")
     footer = page.locator("footer")
     footer.scroll_into_view_if_needed()
     expect(footer).to_be_visible()
-    footer.screenshot(path="footer.png")
+    footer.screenshot(path="screenshots/footer.png")
